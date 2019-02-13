1514d55c69dadbdc92dd3eb9f21471f4c6d35b3f
Vasitha <VasithaKA@users.noreply.github.com>
2019-02-13 11:21:47 +0530

    server

--- api/routes/relation_routes/solves.js
+++ api/routes/relation_routes/solves.js
@@ -115,7 +115,6 @@ router.get('/todayComplete', async (req, res) => {
 //get incompleted Jobs 
 router.get('/incomplete', async (req, res) => {
     const incompletedJobs = await Solve.find({status:"incomplete"},{jobId:1, _id:0})
-    console.log(incompletedJobs)
     var faultsInAJobs = []
     if (incompletedJobs) {
         for (let j = 0; j < incompletedJobs.length; j++) {
